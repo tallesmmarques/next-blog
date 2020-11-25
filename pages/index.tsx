d4bfdbaae6b07a6a37e5b9d21773a31acea2cb6a
Talles Marques <tallesemanuel1@gmail.com>
2020-11-25 14:06:39 -0300

    Primeira requisição e amostragem de produtos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,55 @@
 import Head from 'next/head'
+import axios from 'axios'
+import { Url } from 'url'
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
 
-const Home: React.FC = () => {
+interface IProducts {
+  category: string
+  description: string
+  id: number
+  image: Url
+  price: number
+  title: string
+}
+
+const Home: React.FC = ({
+  products
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <div>
       <Head>
-        <title>Main Page</title>
+        <title>Next Store</title>
       </Head>
 
+      <header>
+        <h1>Next Store</h1>
+      </header>
+
       <main>
-        <h1>Hello Talles</h1>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>{product.title}</li>
+          ))}
+        </ul>
       </main>
+
+      <footer>Talles Marques</footer>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const products: IProducts[] = await axios
+    .get('https://fakestoreapi.com/products')
+    .then((res) => {
+      return res.data
+    })
+
+  return {
+    props: {
+      products
+    }
+  }
 }
 
 export default Home
