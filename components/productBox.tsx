ea2994a03fc7ed94d3637e6936813c7349be54b0
Talles Marques <tallesemanuel1@gmail.com>
2020-11-25 21:23:08 -0300

    Melhorias na proporção das imagens

--- components/productBox.tsx
+++ components/productBox.tsx
@@ -9,12 +9,21 @@ interface ProductsBoxProps {
 const ProdutsBox: React.FC<ProductsBoxProps> = ({ product }) => {
   return (
     <div>
-      <Image src={product.image} alt={product.title} width={200} height={200} />
+      <div style={{ height: '200px', width: '200px', position: 'relative' }}>
+        <Image
+          src={product.image}
+          alt={product.title}
+          layout="fill"
+          objectFit="scale-down"
+        />
+      </div>
+
       <h3>{product.title}</h3>
       <p>{product.description}</p>
       <Link href={`/products/${product.id}`}>
         <a>See more...</a>
       </Link>
+      <hr />
     </div>
   )
 }
